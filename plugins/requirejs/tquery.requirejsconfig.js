091ad2ad78c8aa687072caba9d99507e46bfdd67
sfoster <sam@sam-i-am.com>
2012-06-27 23:21:18 +0100

    Add shim definition for tquery (no deps at this point). (in theory) this should be able to be loaded before *or* after require.js itself

--- plugins/requirejs/tquery.requirejsconfig.js
+++ plugins/requirejs/tquery.requirejsconfig.js
@@ -1,6 +1,25 @@
-var require = {
-	paths	: {
-		'plugins'	: "../../../plugins",
-		'threex'	: "../../../vendor/threex"
+(function(requirejs){
+	var config = {
+		baseUrl: "../../..",
+		enforceDefine: true,
+		paths : {
+			// 'plugins' : "../../../plugins",
+			// 'threex'	: "../../../vendor/threex",
+			'tquery'	: "build/tquery-bundle"
+		},
+		shim : {
+			// any non-AMD mappings can be defined here. See: http://requirejs.org/docs/api.html#config-shim
+			deps: [],
+			tquery: {
+				exports: "tQuery"
+			}
+		} 
+	};
+	if(!requirejs) {
+		// if this is loaded before require.js we just leave our config there where it can find it
+		 requirejs = config;
+	} else {
+		// if requirejs is already defined, pass it our config
+		requirejs.config(config);
 	}
-};
+})(requirejs);
